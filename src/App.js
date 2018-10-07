57888dbc8a8e6113d4950294d2edf7c47dbbd7f5
Krzysztof Domański <domanski.k.a@gmail.com>
2018-10-07 21:57:23 +0100

    Fetching done, css almost done, calculation of rate done.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,86 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-import { Button, DropDownButton, Box, Txt, Provider } from 'rendition';
+import { Button, DropDownButton, Input, Box, Txt, Provider } from 'rendition';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: ''
+            rate: '',
+            selectOptions: []
         }
+    }
+
+    getURL = () => {
+        return 'http://localhost:5000/api/currency'
+    }
+
+    componentDidMount() {
+        this.setUpSelectControls(this.getURL())
+    }
+
+    setUpSelectControls = async (url) => {
+        await fetch(url)
+            .then(data => data.text())
+            .then(data => {
+                const json = JSON.parse(data)
+                let options = []
+                for (let c in json) {
+                    options.push(c)
+                }
+                this.setState({ selectOptions: options })
+            })
+            .catch(err => console.log('Error: ' + err))
     }
 
     fetchData = async (url) => {
         await fetch(url)
             .then(data => data.text())
-            .then(data => this.setState({ data: data }))
+            .then(data => {
+                const primaryCurrency = document.getElementById('primaryCurrency').value
+                const secondaryCurrency = document.getElementById('secondaryCurrency').value
+                const howMuch = document.getElementById('howMuch').value
+
+                const json = JSON.parse(data)
+                const rate = (json[secondaryCurrency] / json[primaryCurrency]) * howMuch
+
+                this.setState({ rate: rate})
+            })
             .catch(err => console.log('Error: ' + err))
     }
 
-    getCurrency = () => {
-        this.fetchData('http://localhost:5000/api/currency')
+    handleGet = () => {
+        this.fetchData(this.getURL())
     }
 
+    // Add onChange in number input and cut if too long
+    // Split form in smaller modules
+    // Use rendition
     render() {
         return (
-            <Provider>
-                <Box m={3}>
-                    <DropDownButton mx={2} primary>
-                        <div>AED</div>
-                        <div>AFN</div>
-                        <div>ALL</div>
-                        <div>AMD</div>
-                    </DropDownButton>
-                    <Button primary onClick={this.getCurrency}>Get</Button>
-                </Box>
-                <Box m={3}>
-                    <Txt mx={2}>{this.state.data}</Txt>
-                </Box>
-            </Provider>
+            <div className='mainContainer'>
+                <div className='formContainer'>
+                    <input type='number' defaultValue='1.00' id='howMuch' className='numberInput' />
+                    
+                    <select id='primaryCurrency' className='selectInput'>
+                        {this.state.selectOptions.map((opt) => <option key={opt} value={opt}>{opt}</option> )}
+                    </select>
+
+                    <div id='equalSign'>=</div>
+
+                    <input type='number' defaultValue={this.state.rate} placeholder='...' readOnly={true} className='numberInput' />
+
+                    <select id='secondaryCurrency' className='selectInput'>
+                        {this.state.selectOptions.map((opt) => <option key={opt} value={opt}>{opt}</option>)}
+                    </select>
+
+                    <input type='button' onClick={this.handleGet} value="Get" className='buttonInput'/>
+                </div>
+                <div className='fotter'>
+                    {this.state.rate}
+                </div>
+            </div>
         );
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,57 @@
 const http = require('http')
+const fs = require('fs')
+const fetch = require('node-fetch')
+const url = require('url')
+
+//http://data.fixer.io/api/latest
+
+//    ? access_key = YOUR_ACCESS_KEY
+//    & base = GBP
+//    & symbols = USD, AUD, CAD, PLN, MXN
+
+//http://data.fixer.io/api/latest?access_key=3978bfe9820d0735b3ef72a64505de38
+
+function fetchData(resp, primary, secondary) {
+    fetch('http://data.fixer.io/api/latest?base=' + primary + '&access_key=3978bfe9820d0735b3ef72a64505de38')
+        .then(res => res.json())
+        .then(res => {
+            fs.writeFile('./data', JSON.stringify(res), function () {
+                console.log('Data written to file.')
+                writeResponse(resp, primary, secondary)
+            })
+        })
+        .catch(err => console.log('Error: ' + err))
+}
+
+function readDataFromFile() {
+    const json = JSON.parse(fs.readFileSync('data', 'utf8'))
+    return JSON.stringify(json.rates)
+}
+
+function writeResponse(resp) {
+    const rates = readDataFromFile()
+    resp.writeHeader(200, { 'content-type': 'text/plain' })
+    resp.write(rates)
+    resp.end()
+}
+
+function needUpdate() {
+    //TODO: Add checking for date and if file exists
+    const json = JSON.parse(fs.readFileSync('data', 'utf8'))
+    return !json.success
+}
 
 function onRequest(req, res) {
-    console.log('Request received: ' + req.method + ' ' + req.url)
+    const url_parts = url.parse(req.url, true);
+    const query = url_parts.query;
+    console.log('Request received: ' + req.method + ' ' + req.url, query)
     res.setHeader('Access-Control-Allow-Origin', '*');
     if (req.url.startsWith('/api/currency')) {
-        res.writeHeader(200, { 'content-type': 'text/plain' })
-        res.write('Hello World!')
-        res.end()
+        if (needUpdate()) {
+            fetchData(res)
+        } else {
+            writeResponse(res)
+        }
     } else {
         res.writeHeader(401, { 'content-type': 'text/plain' })
         res.end()
